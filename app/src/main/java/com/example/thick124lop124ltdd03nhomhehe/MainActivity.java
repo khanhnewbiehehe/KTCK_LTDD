119edd51a818d3d5bf8936579f772b491e4e93c5
NguyenAnhHoangIT <nahoangit@gmail.com>
2024-12-21 11:07:51 +0700

    intent

--- app/src/main/java/com/example/thick124lop124ltdd03nhomhehe/MainActivity.java
+++ app/src/main/java/com/example/thick124lop124ltdd03nhomhehe/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.thick124lop124ltdd03nhomhehe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +11,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.thick124lop124ltdd03nhomhehe.Models.SinhVien;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<SinhVien> sinhvien_listProperties = new ArrayList<>();
+    DatabaseReference mDatabase;
+    private Button btnThem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        btnThem = findViewById(R.id.button7);
+
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, AddMember.class);
+                startActivity(intent1);
+            }
+        });
 
     }
 }
